751871dcadcb67a43cb94818a5d4410b16ce3a20
Xavi Torelló <info@xaviertorello.cat>
2017-10-24 21:33:34 +0200

    Make a preventive rm of the scanner container

--- create.sh
+++ create.sh
@@ -8,11 +8,17 @@ if [ "$#" -ge 1 ]; then
     tag="$1"
 fi
 
+
 # Prepare sockets and logs path
 #mkdir -p $socket_path
 #mkdir -p $SHODAND_SCANNER_logs_destination
 
+# Make a preventive deletion of the container
+docker rm -f $SHODAND_SCANNER_container 2&> /dev/null
+
+# Start the container
 #container_id=`docker run --restart=always --network=host $SHODAND_SCANNER_logs --name $SHODAND_SCANNER_container -i $SHODAND_SCANNER_image:$tag`
+echo docker run --network=host --cap-add=NET_ADMIN --name $SHODAND_SCANNER_container -i $SHODAND_SCANNER_image:$tag
 container_id=`docker run --network=host --cap-add=NET_ADMIN --name $SHODAND_SCANNER_container -i $SHODAND_SCANNER_image:$tag`
 
 if [ "$container_id" != "" ]
