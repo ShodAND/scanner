a5013586b8486ffbf8b841231f8aa0812d4446a1
Xavi Torelló <info@xaviertorello.cat>
2017-10-24 20:46:24 +0200

    Add create container script
    Creates a container named "scanner" using the latest image available or the passed (arg1) one

---
+++ create.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+PATHH=`dirname $0`
+. $PATHH/settings.sh
+
+tag="latest"
+if [ "$#" -ge 1 ]; then
+    tag="$1"
+fi
+
+# Prepare sockets and logs path
+#mkdir -p $socket_path
+#mkdir -p $SHODAND_SCANNER_logs_destination
+
+#container_id=`docker run --restart=always --network=host $SHODAND_SCANNER_logs --name $SHODAND_SCANNER_container -i $SHODAND_SCANNER_image:$tag`
+container_id=`docker run --network=host --cap-add=NET_ADMIN --name $SHODAND_SCANNER_container -i $SHODAND_SCANNER_image:$tag`
+
+if [ "$container_id" != "" ]
+then
+    chmod -R 777 $socket_path
+    echo "$container_id" > $SHODAND_SCANNER_pid
+    echo "$SHODAND_SCANNER_container started! id: "$container_id
+else
+    if [ -e "id_frontend" ]; then
+        rm $SHODAND_SCANNER_pid
+    fi
+    echo "[Error] $SHODAND_SCANNER_container can't be started!"
+fi
+
