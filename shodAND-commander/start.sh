bbd0b45354e87bf28234602a442ab54186ffb72f
Xavi Torelló <info@xaviertorello.cat>
2017-11-26 21:54:00 +0100

    Starting processes in quiet mode
    This avoids to create the nohub.out file for each process

--- shodAND-commander/start.sh
+++ shodAND-commander/start.sh
@@ -2,6 +2,6 @@
 
 for process in "receiver" "processor" "transmitter"; do
     echo "Starting $process in background"
-    nohup sh -c /opt/commander/$process.py &
+    nohup sh -c /opt/commander/$process.py >/dev/null 2>&1 &
 done
 
